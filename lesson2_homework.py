cf8e708557c330d4ca75c32d302f2bb793863ebd
denisbazavliuk <denis.bazavliuk@gmail.com>
2016-02-13 13:00:17 +0600

    lesson 2 homework compl

--- lesson2_homework.py
+++ lesson2_homework.py
@@ -1,3 +1,30 @@
 #coding=utf-8
 
-print ('Не сделал')
+#1
+string = ("London is a capital of GB")
+z = string.split(" ")
+print z
+print(max(z, key=len))
+
+#2
+string2 = ("London;is;a;capital;of;GB")
+z = string2.split(';')
+print z
+print (max(z, key=len))
+
+#3
+string3 = str(raw_input('Введите знак'))                # не понятно
+string4 = ('London is%s a capital of GB')% (string3)
+z = string4.split(string3)
+print (min(z, key=len))
+
+#4
+a = str(raw_input("Введите предложение: "))
+b = str(raw_input('Введите слово для поиска: '))
+c = (a[a.find(b): len(b)])
+print c
+
+#5
+a = str(raw_input('Введите предложение: '))
+b = a.split(' ')
+print len(b)
