17cf76ba3fc4118cbf0cee9eaffc49bd7a5606e9
denisbazavliuk <denis.bazavliuk@gmail.com>
2016-02-25 20:36:55 +0600

    made some changes

--- lesson 1 homework.py
+++ lesson 1 homework.py
@@ -1,38 +1,39 @@
 # coding=utf-8
+
 # вторая задача
 print ("Общество в начале XXI века")
-a = int(input('Укажите Ваш возраст: '))
-if a<=7:
+inp_age = int(input('Укажите Ваш возраст: '))
+if inp_age <= 7:
     print ('Вам в детский сад')
-elif (a >= 7 and a <=18):
+elif 7 >= inp_age <= 18:
     print ('Вам в школу')
-elif (a >= 18 and a <= 25):
+elif 18 >= inp_age <= 25:
     print ('Вам в профессиональное учебное заведение')
-elif (a >=25 and a <= 60):
+elif 25 >= inp_age <= 60:
     print ('Вам на работу')
-elif (a >=60 and a <=120):
+elif 60 >= inp_age <= 120:
     print ('Вам предоставляется выбор')
-elif (a < 0 or a > 120):
-    x = ('Ошибка! Это программа для людей!\n')
-    print (x * 5)
+elif 0 < inp_age > 120:
+    error = 'Ошибка! Это программа для людей!\n'
+    print (error * 5)
 
 # первая задача
 x = int(input('Введите число от 1 до 9: '))
-if (x >= 1 and x <= 3):
+if 1 <= x <= 3:
     s = str(raw_input("введите строку: "))
     n = int(input('введите число повторов строки: '))
     i = 0
     while i < n:
         print s
-        i = i + 1
-elif (x >=4 and x <=6):
+        i = (i + 1)
+elif 4 <= x <= 6:
     m = int(input('Введите степень в которую нужно возвести число: '))
-    print x ** m
-elif (x >=7 and x <=9):
+    print (x ** m)
+elif 7 <= x <= 9:
     j = 0
     while j < 10:
-        x = x + 1
+        x = (x + 1)
         print x
-        j = j +1
-elif (x > 10):
+        j = (j + 1)
+elif x > 10:
     print ('Ошибка')
